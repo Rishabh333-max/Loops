67f80910d8ba9f13869066618bad8375c5bf2fca
Rishabh333-max <81370987+Rishabh333-max@users.noreply.github.com>
2021-06-13 23:23:00 +0530

    loops

---
+++ js/Rishabh.js
@@ -0,0 +1,124 @@
+// FOR LOOP---------->
+
+// for(let i=0; i<100; i++){
+//     console.log(i);
+// }
+
+// for(let i=0; i<100; i++){
+//     console.log(i+2);
+// }
+
+// WHILE LOOP---------->
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i+=1;
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log(i+2);
+//     i+2;
+// }
+
+
+// let i=0;
+// while(i<10){
+//     console.log(i+10);
+//     i+=1;
+// }
+
+// let j=0;
+// while(j<10){
+    
+//     console.log(j+1);
+//     j+=1;
+// }
+
+//  DO WHILE----------->
+
+// let k=0;
+// do{
+//     console.log(k);
+//     k+=1;
+// }
+// while(k<10);
+
+
+// let i=0;
+// do{
+//     console.log(i+3);
+//     i+=1;
+// }
+// while(i<10)
+
+// BREAK------------>
+
+// let k=0;
+// do{
+//     console.log(k);
+//     if(k===5){
+//         break;
+//     }
+//     k+=1;
+// }while(k<10)
+
+// let k=0;
+// do{
+//     console.log(k);
+//     if(k===2){
+//         break;
+//     }
+//     k+=1;
+// }while(k<10);
+// console.log("done")
+
+// CONTINUE------------>
+
+// let k=0;
+// do{
+//     if(k===5){continue;}
+//         console.log(k);
+//         k+=1;
+// }while(k<10);
+// console.log("DONE")
+
+// let k=0;
+// do{
+//     if(k===5){
+//         k+=1;
+//         continue;
+//     }
+//     console.log(k+1);
+//     k+=1;
+// }while(k<10);
+// console.log("DONE")
+
+// let k=0;
+// do{
+//     console.log(k);
+//     if(k===4){
+//         continue;
+//     }
+//     k+=1;
+// }while(k<10);
+// console.log("done");
+
+// USE OF arr.forEach()--------->
+
+
+// let arr=[2,3,4,5,6];
+// for(let i=0;i<arr.length;i++){
+//     const element=arr[i];
+//     console.log(element);
+// }
+// console.log("Rishabh")
+
+//          OR
+// let arr=[2,3,4,5,6];
+// arr.forEach(function(element){
+//     console.log(element);
+// }
+// );
+// console.log("DONE")
